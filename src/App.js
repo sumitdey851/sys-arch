2ec58db47b8045331fc728a88c1b710b8020b5ba
Sumit Dey <sumitdey851@outlook.com>
2021-09-19 22:28:08 +0530

    Integrated login API

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Layout from "./components/Layout/Layout";
 import AuthPage from "./pages/AuthPage";
 // import HomePage from "./pages/HomePage";
 import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
-import AppAuth from "./store/authContext";
 
 const HomePage = React.lazy(() => import("./pages/HomePage"));
 const UserProfile = React.lazy(() =>
@@ -14,23 +13,21 @@ const UserProfile = React.lazy(() =>
 
 function App() {
   return (
-    <AppAuth.Provider>
-      <Layout>
-        <Suspense fallback={<LoadingSpinner />}>
-          <Switch>
-            <Route path="/" exact>
-              <HomePage />
-            </Route>
-            <Route path="/auth">
-              <AuthPage />
-            </Route>
-            <Route path="/profile">
-              <UserProfile />
-            </Route>
-          </Switch>
-        </Suspense>
-      </Layout>
-    </AppAuth.Provider>
+    <Layout>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+          <Route path="/profile">
+            <UserProfile />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Layout>
   );
 }
 
---
+++ src/components/Auth/loginHandler.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const loginHandler = async (appAuthToken, enteredEmail, enteredPassword) => {
+  const response = await axios({
+    method: "post",
+    url: "https://sys-arch-security-layer.herokuapp.com/sysArch/api",
+    data: {
+      header: {
+        appAuthToken,
+        userAuthToken: "",
+      },
+      params: {
+        method: "post",
+        requestType: "login",
+        role: "",
+      },
+      reqObj: {
+        email: enteredEmail,
+        password: enteredPassword,
+      },
+    },
+  });
+  return response.data;
+};
+
+export default loginHandler;
