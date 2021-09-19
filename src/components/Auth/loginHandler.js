d8ebdb506f1c79a10a4e6d1fe98f20c1e0253cbd
Sumit Dey <sumitdey851@outlook.com>
2021-09-19 23:23:24 +0530

    Debug

--- src/components/Auth/loginHandler.js
+++ src/components/Auth/loginHandler.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const loginHandler = async (appAuthToken, enteredEmail, enteredPassword) => {
   console.log("Calling login API");
+  console.log(appAuthToken);
+  console.log(enteredEmail);
+  console.log(enteredPassword);
   const response = await axios({
     method: "post",
     url: "https://sys-arch-security-layer.herokuapp.com/sysArch/api",
